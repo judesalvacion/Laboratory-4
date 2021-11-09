5c13b079c07f8cb4af3fd01938107a7c1eb7519f
Teacher <salvacionjudd@gmail.com>
2021-11-09 23:01:29 +0800

    Laboratory 4

---
+++ app/src/main/java/com/example/a4/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.a4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    int costPerTicket = 200;
+    int numberOfTickets;
+    int totalCost;
+    String groupChoice;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText ticket = (EditText) findViewById(R.id.tickets);
+        Spinner group = (Spinner) findViewById(R.id.spinner);
+        TextView result = (TextView) findViewById(R.id.result);
+
+        Button cost = (Button) findViewById(R.id.findcost);
+        cost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                numberOfTickets = Integer.parseInt(ticket.getText().toString());
+                totalCost = costPerTicket * numberOfTickets;
+                groupChoice = group.getSelectedItem().toString();
+                result.setText("Cost of fare " + groupChoice + " is " + totalCost + " pesos.");
+
+
+            }
+        });
+
+    }
+
+}
